0ad5ad646482bccc775cfa3e2d8bb13a6436a735
charlescolbourn <charles.colbourn@gmail.com>
2018-10-27 14:17:47 +0100

    populating widget with photo/name

--- app/src/main/java/net/colbourn/carepriorities/ui/SelectClient.java
+++ app/src/main/java/net/colbourn/carepriorities/ui/SelectClient.java
@@ -7,10 +7,14 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import net.colbourn.carepriorities.R;
 import net.colbourn.carepriorities.model.api.Person;
@@ -18,12 +22,15 @@ import net.colbourn.carepriorities.model.Client;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class SelectClient extends AppCompatActivity {
 
     private static final String L = SelectClient.class.getName();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,8 +47,10 @@ public class SelectClient extends AppCompatActivity {
 //                        .setAction("Action", null).show();
 //            }
 //        });
+
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         showListOfClients();
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     private List<Person> getAllClients() {
@@ -64,38 +73,101 @@ public class SelectClient extends AppCompatActivity {
         return retList;
     }
 
-    //TODO - lifted this method from some old code. Clean up
-    private void showListOfClients(){
-        final ListView view = (ListView) findViewById(R.id.listOfClients);
+
+
+    private void showListOfClients()
+    {
+        setContentView(R.layout.activity_select_client);
+
         final List<Person> clients = getAllClients();
 
-        List<String> clientnames = getClientNames(clients);
 
+        List<HashMap<String, String>> aList = new ArrayList<HashMap<String, String>>();
 
-        ArrayAdapter<String> myarrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, clientnames);
-        view.setAdapter(myarrayAdapter);
-        view.setTextFilterEnabled(true);
+        for (Person p: clients) {
+            HashMap<String, String> hm = new HashMap<String, String>();
+            hm.put("listview_title", p.getName());
+            hm.put("listview_description", "");
+            hm.put("listview_image", Integer.toString(R.drawable.dumpling)); // quick demo hack
+            aList.add(hm);
+        }
 
-        view.setClickable(true);
-        view.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        String[] from = {"listview_image", "listview_title", "listview_description"};
+        int[] to = {R.id.listview_image, R.id.listview_item_title, R.id.listview_item_short_description};
 
+        SimpleAdapter simpleAdapter = new SimpleAdapter(getBaseContext(), aList, R.layout.listview_textandphoto, from, to);
+        ListView clientListView = (ListView) findViewById(R.id.listOfClients);
+        clientListView.setAdapter(simpleAdapter);
+        clientListView.setClickable(true);
+        clientListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> av, View arg1, int i, long arg3) {
                 Log.v(L, "client at pos " + i);
-                String p = (String) view.getItemAtPosition(i);
-                Log.v(L, "Client  " + p);
-                this.openDiary(clients.get(i));
+                HashMap<String,String> p = (HashMap<String,String>) clientListView.getItemAtPosition(i);
+                Log.v(L, "Client  " + p.get("listview_title"));
+//                openDiary(clients.get(i));
                 //finish();
             }
-
-            private void openDiary(Person person) {
-//                Intent intent = new Intent(SelectClient.this,ClientDiary.class);
-//                Log.v(SelectClient.class.toString(),"Selecting user " + person.displayname);
-//                Log.v(SelectClient.class.toString(),"uid " + ( person.id != null ? "present" + person.id.toString() : "absent"));
-//                intent.putExtra("CLIENT_ID", person.id);
-//                startActivity(intent);
-            }
         });
+    }
+
+    //TODO - lifted this method from some old code. Clean up
+//    private void showListOfClients(){
+//        final ListView view = (ListView) findViewById(R.id.listOfClients);
+
+//
+//
+//        ArrayAdapter<String> myarrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, clientnames);
+//        view.setAdapter(myarrayAdapter);
+//        view.setTextFilterEnabled(true);
+//
+//
+//
+//            private void openDiary(Person person) {
+////                Intent intent = new Intent(SelectClient.this,ClientDiary.class);
+////                Log.v(SelectClient.class.toString(),"Selecting user " + person.displayname);
+////                Log.v(SelectClient.class.toString(),"uid " + ( person.id != null ? "present" + person.id.toString() : "absent"));
+////                intent.putExtra("CLIENT_ID", person.id);
+////                startActivity(intent);
+//            }
+//        });
+//
+//    }
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent intent = null;
+        switch (item.getItemId()) {
+            case R.id.createclient:
+//                intent = new Intent(this,EditClient.class);
+//                startActivity(intent);
+                return true;
+            case R.id.help:
+//                intent = new Intent(this,HelpActivity.class);
+//                intent.putExtra("HELP_TITLE",R.string.help_selectclient_title);
+//                intent.putExtra("HELP_TEXT",R.string.help_selectclient_text);
+                startActivity(intent);
+                return true;
+            case R.id.setupapplication:
+//                intent = new Intent(this,Setup.class);
+//                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_select_client, menu);
+        return true;
 
     }
+
 }
