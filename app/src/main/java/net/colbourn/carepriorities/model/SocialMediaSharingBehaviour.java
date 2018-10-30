a2fd319bce829c4f0158123c959f60f5243eb869
Charles Colbourn <charles.colbourn@lhasalimited.org>
2018-10-30 12:50:18 +0000

    added SocialMediaSharingBehaviour class. added some notes to PlannedStories.txt

---
+++ app/src/main/java/net/colbourn/carepriorities/model/SocialMediaSharingBehaviour.java
@@ -0,0 +1,15 @@
+/***************************************************************************************************
+ * Copyright C W Colbourn 2018
+ *
+ * Licence: GPL
+ *
+ **************************************************************************************************/
+package net.colbourn.carepriorities.model;
+
+import java.io.Serializable;
+
+class SocialMediaSharingBehaviour implements Serializable
+{
+    private Boolean shareable;
+    private Boolean autoShare;
+}
